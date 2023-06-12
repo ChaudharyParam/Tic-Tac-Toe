d20d1eb3e80b869433d6f2e2015cfab3019d7c54
Param Chaudhary <chaudharyparam678@gmail.com>
2023-06-12 13:33:59 +0530

    added init function

---
+++ app.js
@@ -0,0 +1,31 @@
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+const winningCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+const cells = document.querySelectorAll("td");
+
+const Winner = document.querySelector(".winner")
+
+let winnerDeclared = false;
+
+function handleClick(e) {
+}
+
+
+function init() {
+
+    cells.forEach((cell) => {
+        cell.addEventListener("click", handleClick);
+    });
+}
+
+init();
